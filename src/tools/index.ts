95bdfec1059006aff4f349243cdd7f0cb5a0e0b3
Dave Nicholl <dnicholl@objective.works>
2025-12-02 12:11:50 -0600

    Force rebuild - fix null bytes

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -3,3 +3,4 @@ export { sendLetterTool } from "./sendLetter.js";
 export { getOrderStatusTool } from "./getOrderStatus.js";
 export { getAccountBalanceTool } from "./getAccountBalance.js";
 export { listOrdersTool } from "./listOrders.js";
+// Force rebuild Tue Dec  2 12:11:48 CST 2025
