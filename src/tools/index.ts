12114bd58bb1b69312054a5600d0569fee85ef29
Dave Nicholl <dnicholl@objective.works>
2025-12-03 23:42:33 -0600

    Force Railway rebuild - clear cache

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -3,4 +3,4 @@ export { sendLetterTool } from "./sendLetter.js";
 export { getOrderStatusTool } from "./getOrderStatus.js";
 export { getAccountBalanceTool } from "./getAccountBalance.js";
 export { listOrdersTool } from "./listOrders.js";
-// Force rebuild Tue Dec  2 12:11:48 CST 2025
+// Force rebuild Wed Dec  4 00:00:00 CST 2025
